e5ea2abe776175b9a4594aee0ebd5a615fb8037e
Ferytom <garuda.bangkit@gmail.com>
2023-09-05 08:14:45 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,14 +19,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::view('/about', 'about')->name('about');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('admin')->middleware('admin')->group(function () {
+        Route::view('/dashboard', 'admin.dashboard')->name('admin.dashboard');
+        Route::view('/ecommerce', 'admin.ecommerce')->name('admin.ecommerce');
+        Route::view('/alert', 'admin.alert')->name('admin.alert');
+        Route::view('/email', 'admin.email')->name('admin.email');
+        Route::resource('categories', CategoryController::class)->except(['show']);
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
